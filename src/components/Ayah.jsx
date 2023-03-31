71cda3c65de42dc52a25051de8c116f4e6e224c1
kazi331 <kazisharifulislam52@gmail.com>
2023-03-31 18:09:56 +0600

    framer removed

--- src/components/Ayah.jsx
+++ src/components/Ayah.jsx
@@ -1,8 +1,6 @@
-import { motion } from "framer-motion";
 import React, { useRef, useState } from "react";
 import ReactPlayer from "react-player";
 import { useSelector } from "react-redux";
-import { saveAsJpeg } from 'save-html-as-image';
 
 import v from "../assets/images/image00.jpg";
 import { default as a, default as q } from "../assets/images/image2.jpg";
@@ -49,14 +47,14 @@ const Ayah = () => {
   // screenshot download
   const imgRef = useRef();
   const canvasRef = useRef();
- 
+
 
 
   // random ayah 
   // let ayahNumber = Math.floor(Math.random() * 6236) + 1;
 
   const { data: randomAyah } = useRandomAudioAyahQuery(ayahNumber);
-  const { data: randomEnglish} = useRandomEnglishAyahQuery(ayahNumber);
+  const { data: randomEnglish, isLoading } = useRandomEnglishAyahQuery(ayahNumber);
 
   const refresh = () => {
     setAyahNumber(Math.floor(Math.random() * 6236) + 1);
@@ -70,56 +68,57 @@ const Ayah = () => {
       <canvas ref={canvasRef} style={{ display: 'none' }}></canvas>
       <div
         ref={imgRef}
-        className="flex items-center justify-center min-h-screen bg-gray-900"
+        className="p-4 flex items-center justify-center min-h-screen bg-gray-900"
         style={{
           backgroundImage: `url('${activeImage}')`,
           backgroundSize: "cover",
         }}
       >
-        <motion.div
+        <div className={`${isLoading ? "w-96 h-96 overflow-hidden rounded" : "w-full h-full"}  transition-all duration-1000`}>
+          <div
+            // initial={{ scale: 0 }}
+            // animate={{ scale: [0.5, 1.2, 1.2, 1, 1] }}
+            // transition={{ duration: 2 }}
+            className="max-w-5xl mx-auto  rounded-3xl shadow-xl w-full  backdrop-filter  backdrop-blur-lg bg-opacity-10 bg-gray-900"
+          // style={{ backgroundImage: `url('${activeImage}')` }}
+          >
+            <div className="bg-fixed" >
+              <div className="flex flex-col space-y-4 h-full p-4 md:p-6 lg:p-8 justify-between text-center ">
+                {/* main content  */}
 
-          initial={{ scale: 0 }}
-          animate={{ scale: [0.5, 1.2, 1.2, 1, 1] }}
-          transition={{ duration: 2 }}
-          className="max-w-5xl m-2 md:m-4 lg:m-6  rounded-3xl shadow-xl w-full   backdrop-filter  backdrop-blur-lg bg-opacity-10 bg-gray-900"
-        // style={{ backgroundImage: `url('${activeImage}')` }}
-        >
-          <div className="bg-fixed" >
-            <div className="flex flex-col space-y-4 h-full p-4 md:p-6 lg:p-8 justify-between text-center ">
-              {/* main content  */}
+                <div className=" text-1xl font-medium text-white text-center 	">
+                  <p > {audio?.surah?.name}</p>
+                  <div className="  text-xs font-small text-white text-center">
+                    {audio?.surah?.englishName} - {audio?.surah?.englishNameTranslation}
+                  </div>
 
-              <div className="   text-1xl font-medium text-white text-center 	">
-                <p > {audio?.surah?.name}</p>
-                <div className="  text-xs font-small text-white text-center	m-2">
-                  {audio?.surah?.englishName} - {audio?.surah?.englishNameTranslation}
+                  <div className=" flex  flex-col gap-3   text-white	 text-center 	mt-4">
+                    <h2 className="main-text text-xl mb-4" style={{ writingDirection: "rlt" }} >{audio?.text}</h2>
+                    <p >-{english?.text ? english.text : "Click the refresh icon below to reveal an Ayah"}</p>
+                    <hr className="w-2/12 md:w-1/12 mx-auto mt-10" />
+                    <h1 >{english?.surah?.revelationType} Ayah</h1>
+                    <h5 className="text-center pb-2.5    text-xs text-white text-opacity-50		"> - {english?.surah?.number}:{english?.numberInSurah} - </h5>
+                  </div>
+
+                  {/* controls  */}
+                  <Controls props={{ setShowModal, canvasRef, imgRef, refresh, play, setPlay, randomizeBg }} />
+
+                  <ReactPlayer
+                    url={audio?.audio}
+                    playing={play}
+                    height={0}
+                    width={0}
+                    onEnded={() => setPlay(false)}
+                  />
+                  <p className="text-gray-100  text-sm text-centre ">Aayah.app</p>
                 </div>
 
-                <div className=" flex  flex-col gap-3   text-white	 text-center 	mt-4">
-                  <h2 className="main-text text-2xl mb-4" style={{ writingDirection: "rlt" }} >{audio?.text}</h2>
-                  <p >-{english?.text ? english.text : "Click the refresh icon below to reveal an Ayah"}</p>
-                  <hr className="w-2/12 md:w-1/12 mx-auto mt-10" />
-                  <h1 >{english?.surah?.revelationType} Ayah</h1>
-                  <h5 className="text-center pb-2.5    text-xs text-white text-opacity-50		"> - {english?.surah?.number}:{english?.numberInSurah} - </h5>
-                </div>
 
-                {/* controls  */}
-                <Controls props={{ setShowModal, canvasRef, imgRef, refresh, play, setPlay, randomizeBg }} />
-
-                <ReactPlayer
-                  url={audio?.audio}
-                  playing={play}
-                  height={0}
-                  width={0}
-                  onEnded={() => setPlay(false)}
-                />
-                <p className="text-gray-100  text-sm text-centre ">Aayah.app</p>
               </div>
-
-
             </div>
           </div>
-        </motion.div>
-       {showModal ? <SearchAyah showModal={showModal} setShowModal={setShowModal} /> : null}
+        </div>
+        {showModal ? <SearchAyah showModal={showModal} setShowModal={setShowModal} /> : null}
       </div>
 
     </>
--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { saveAsJpeg } from 'save-html-as-image';
-import { Download, Pause, Play, Refresh, Screenshot, Search, Share } from './Icons';
-import { useRef } from 'react';
-import html2canvas from 'html2canvas';
+import { Download, Pause, Play, Refresh, Screenshot, Search } from './Icons';
 
 
 
 
 const Controls = ({ props }) => {
     const { play, setPlay, refresh, imgRef, canvasRef, randomizeBg, setShowModal } = props;
+
     const downloadImg = () => {
         saveAsJpeg(imgRef.current, {
             fileName: "ayah",
@@ -16,41 +15,6 @@ const Controls = ({ props }) => {
         });
     };
 
-    function captureScreenshot() {
-        const canvas = canvasRef.current;
-        const context = canvas.getContext('2d');
-
-        const width = window.innerWidth;
-        const height = window.innerHeight;
-
-        canvas.width = width;
-        canvas.height = height;
-
-        return new Promise(resolve => {
-            html2canvas(document.body).then(canvas => {
-                const imageData = canvas.toDataURL('image/png');
-                resolve(imageData);
-            });
-        });
-    }
-
-
-   
-  
-    async function shareImage() {
-        try {
-            const imageData = await captureScreenshot();
-            await navigator.share({
-                title: 'My Image',
-                text: 'Check out this cool image!',
-                files: [
-                    new File([imageData], 'image.png', { type: 'image/png' }),
-                ],
-            });
-        } catch (error) {
-            console.error('Error sharing image:', error);
-        }
-    }
 
 
 
@@ -80,10 +44,6 @@ const Controls = ({ props }) => {
 
             <button className=" p-2 outline-none md:hover:bg-gray-300 hover:bg-opacity-20 rounded md:hover:-translate-y-1 md:hover:scale-110 transition-all" onClick={randomizeBg} >
                 <Screenshot className="w-7 h-7 md:w-5 md:h-5" />
-
-            </button>
-            <button className=" p-2 outline-none md:hover:bg-gray-300 hover:bg-opacity-20 rounded md:hover:-translate-y-1 md:hover:scale-110 transition-all" onClick={shareImage} >
-                <Share className="w-7 h-7 md:w-5 md:h-5" />
 
             </button>
         </div>

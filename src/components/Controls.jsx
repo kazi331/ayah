02eda9de7fe055274b12e2bed2e89b56cc0ab17a
kazi331 <kazisharifulislam52@gmail.com>
2023-03-31 17:24:18 +0600

    bg and scale removed for mobile

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -39,33 +39,33 @@ const Controls = ({ props }) => {
 
     return (
         <div className='flex gap-1 items-center justify-center my-3'>
-            <button className=" p-2 hover:bg-gray-300 hover:bg-opacity-20 rounded hover:-translate-y-1 hover:scale-110 transition-all" type="button" onClick={refresh} >
-                <Refresh className="w-7 h-7 md:w-5 md:h-5" />
+            <button className=" p-2 md:hover:bg-gray-300 hover:bg-opacity-20 rounded md:hover:-translate-y-1 md:hover:scale-110 transition-all" type="button" onClick={refresh} >
+                <Refresh className="w-7 h-7 md:w-5 md:h-5 mt-1" />
             </button>
-            <button className=" p-2 hover:bg-gray-300 hover:bg-opacity-20 rounded hover:-translate-y-1 hover:scale-110 transition-all" onClick={downloadImg} >
+            <button className=" p-2 md:hover:bg-gray-300 hover:bg-opacity-20 rounded md:hover:-translate-y-1 md:hover:scale-110 transition-all" onClick={downloadImg} >
                 <Download className="w-7 h-7 md:w-5 md:h-5" />
             </button>
-            <button className=" p-2 hover:bg-gray-300 hover:bg-opacity-20 rounded hover:-translate-y-1 hover:scale-110 transition-all" onClick={() => setShowModal(true)} >
+            <button className=" p-2 md:hover:bg-gray-300 hover:bg-opacity-20 rounded md:hover:-translate-y-1 md:hover:scale-110 transition-all" onClick={() => setShowModal(true)} >
                 <Search className="w-7 h-7 md:w-5 md:h-5" />
 
             </button>
 
             {
                 play === false ?
-                    <button className=" p-2 hover:bg-gray-300 hover:bg-opacity-20 rounded hover:-translate-y-1 hover:scale-110 transition-all" onClick={() => setPlay(true)} >
+                    <button className=" p-2 md:hover:bg-gray-300 hover:bg-opacity-20 rounded md:hover:-translate-y-1 md:hover:scale-110 transition-all" onClick={() => setPlay(true)} >
                         <Play className="w-7 h-7 md:w-5 md:h-5" />
                     </button>
                     :
-                    <button className=" p-2 hover:bg-gray-300 hover:bg-opacity-20 rounded hover:-translate-y-1 hover:scale-110 transition-all" onClick={() => setPlay(false)} >
+                    <button className=" p-2 md:hover:bg-gray-300 hover:bg-opacity-20 rounded md:hover:-translate-y-1 md:hover:scale-110 transition-all" onClick={() => setPlay(false)} >
                         <Pause className="w-7 h-7 md:w-5 md:h-5" />
                     </button>
             }
 
-            <button className=" p-2 hover:bg-gray-300 hover:bg-opacity-20 rounded hover:-translate-y-1 hover:scale-110 transition-all" onClick={randomizeBg} >
+            <button className=" p-2 md:hover:bg-gray-300 hover:bg-opacity-20 rounded md:hover:-translate-y-1 md:hover:scale-110 transition-all" onClick={randomizeBg} >
                 <Screenshot className="w-7 h-7 md:w-5 md:h-5" />
 
             </button>
-            <button className=" p-2 hover:bg-gray-300 hover:bg-opacity-20 rounded hover:-translate-y-1 hover:scale-110 transition-all" onClick={handleShare} >
+            <button className=" p-2 md:hover:bg-gray-300 hover:bg-opacity-20 rounded md:hover:-translate-y-1 md:hover:scale-110 transition-all" onClick={handleShare} >
                 <Share className="w-7 h-7 md:w-5 md:h-5" />
 
             </button>
